daa43fe91824ad935d4aac6f08befe744e412ddb
zhaolei <zhaolei@allinmd.cn>
2016-03-10 01:12:28 +0800

    init SubmoduleLogin project

---
+++ SubmoduleLogin/SubmoduleLogin.h
@@ -0,0 +1,13 @@
+//
+//  SubmoduleLogin.h
+//  SubmoduleLogin
+//
+//  Created by wdxgtsh on 16/3/10.
+//  Copyright © 2016年 zhaolei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SubmoduleLogin : NSObject
+
+@end
---
+++ SubmoduleLogin/SubmoduleLogin.m
@@ -0,0 +1,13 @@
+//
+//  SubmoduleLogin.m
+//  SubmoduleLogin
+//
+//  Created by wdxgtsh on 16/3/10.
+//  Copyright © 2016年 zhaolei. All rights reserved.
+//
+
+#import "SubmoduleLogin.h"
+
+@implementation SubmoduleLogin
+
+@end
